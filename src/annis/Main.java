d004a0fea8c4f75cfeab363a595b85a5cad341c5
abbyannis <abbyannis@gmail.com>
2020-10-06 22:02:35 -0600

    Always forgetting the semicolons!

--- src/annis/Main.java
+++ src/annis/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is a test.");
+        System.out.println("Adding one in the middle.");
         System.out.println("Another test.");
     }
 }
