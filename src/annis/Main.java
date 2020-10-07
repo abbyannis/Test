390742c906bc2ac1a77d3dc94e72b290574dce49
abbyannis <abbyannis@gmail.com>
2020-10-06 22:03:34 -0600

    Just adding commits

--- src/annis/Main.java
+++ src/annis/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a test.");
         System.out.println("Adding one in the middle.");
+        System.out.println("Another one in the middle.");
         System.out.println("Another test.");
     }
 }
